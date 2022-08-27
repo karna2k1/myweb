e56d4d6e402b168256ac002b3b861ee88a4b84ee
karna2k1 <karunakarreddy2001@gmail.com>
2022-08-27 23:14:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let hi=document.getElementById(" hello");
+function display(){
+    document.getElementsByTagName("h3")[0].innerHTML="hello";
+}
+function hello() {
+    alert('hello');
+}
+function come(){
+    alert('come');
+}
+function who(){
+    
+}
